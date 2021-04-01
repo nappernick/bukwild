44d1b48b0d979b984e3010cd7461f7d8decc183b
nappernick <nickfmatthews@gmail.com>
2021-04-01 17:18:03 -0400

    Annotating code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import './App.css';
 import Layout from "./Layout/Layout"
 import data from "./assets/JSONData"
-import { setTitleTag } from "./utils"
+import { setTitleTag, buildJsonData } from "./utils"
 import Page from "./Page/Page";
 
 function App() {
@@ -11,48 +11,37 @@ function App() {
   const [selected, setSelected] = useState("")
   const [layoutData, setLayoutData] = useState([])
   const [pageData, setPageData] = useState([])
+  // Defaults to "abc" so it's not blank during initial load
   const [title, setTitle] = useState("abc")
 
+  // Save on component prop fields by consolidating into a single object
   const consolidatedObj = {
     "setSelected": setSelected,
     "setTitle": setTitle,
     "selected": selected,
     "layoutData": layoutData
   }
-
+  // Cleaned up long use effect function to buildJsonData in utils
   useEffect(() => {
     if (jsonData.length) {
-      let layoutDataArray = []
-      let pageDataArray = {}
-      jsonData.forEach(page => {
-        layoutDataArray.push({
-          "title": page.title,
-          "slug": page.slug
-        })
-        pageDataArray[page.slug] = {
-          "type": page.blocks[0].type,
-          "headline": page.blocks[0].headline,
-          "subhead": page.blocks[0].subhead,
-          "cta": page.blocks[0].cta,
-          "background": page.blocks[0].background,
-          "slug": page.slug
-        }
-      })
-      setLayoutData(layoutDataArray)
-      setPageData(pageDataArray)
-      setSelected(jsonData[0].slug)
-      setTitle(jsonData[0].title)
+      buildJsonData(jsonData, setLayoutData, setPageData, setSelected, setTitle)
     }
   }, [jsonData])
 
+  // This sets the text that updates the page title
   useEffect(() => {
     setTitleTag(title)
   }, [title])
 
+  // This actually updates the title element in the head
   useEffect(() => {
     setJsonData(data.pages)
     setTitleTag(title)
   }, [title])
+
+  // For faster image load, could create useEffect that fetches all images on 
+  // page load, instead of requiring the browser to fetch each as the user
+  // navigates between pages
 
   return (
     <Router>
--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,26 @@ export const setTitleTag = (title) => {
     const titleElement = document.getElementsByTagName("title")[0]
     titleElement.innerHTML = title
 }
+
+export const buildJsonData = (jsonData, setLayoutData, setPageData, setSelected, setTitle) => {
+    let layoutDataArray = []
+    let pageDataArray = {}
+    jsonData.forEach(page => {
+        layoutDataArray.push({
+            "title": page.title,
+            "slug": page.slug
+        })
+        pageDataArray[page.slug] = {
+            "type": page.blocks[0].type,
+            "headline": page.blocks[0].headline,
+            "subhead": page.blocks[0].subhead,
+            "cta": page.blocks[0].cta,
+            "background": page.blocks[0].background,
+            "slug": page.slug
+        }
+    })
+    setLayoutData(layoutDataArray)
+    setPageData(pageDataArray)
+    setSelected(jsonData[0].slug)
+    setTitle(jsonData[0].title)
+}
